ac277f97ab5faeb055d3a83a00446906f507110a
Yashwanth reddy <yash868686reddy@gmail.com>
2022-10-31 18:53:09 -0400

    HI

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,22 @@ import { Router } from 'express'
 
 const router = Router()
 
+router.route('/')
+    .get((req, res) => {
+        res.send({ message: "hello" })
+    })
+    .post((req, res) => {
+        res.send({ message: "hello" })
+    })
+router.route('/:id')
+    .get((req, res) => {
+        res.send({ message: "hello" })
+    })
+    .put((req, res) => {
+        res.send({ message: "hello" })
+    })
+    .delete((req, res) => {
+        res.send({ message: "hello" })
+    })
+
 export default router
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import config from './config'
 import cors from 'cors'
 import { connect } from './utils/db'
+import itemRouter from './resources/item/item.router/'
 
 export const app = express()
 
@@ -14,13 +15,15 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+app.use('/api/item', itemRouter);
+
 export const start = async () => {
-  try {
-    await connect()
-    app.listen(config.port, () => {
-      console.log(`REST API on http://localhost:${config.port}/api`)
-    })
-  } catch (e) {
-    console.error(e)
-  }
+    try {
+        await connect()
+        app.listen(config.port, () => {
+            console.log(`REST API on http://localhost:${config.port}/api`)
+        })
+    } catch (e) {
+        console.error(e)
+    }
 }
